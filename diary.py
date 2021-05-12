b049c92d43bda21b6f86d5de39c4e4434ce0b956
AkashShanmugaraj <69229736+AkashShanmugaraj@users.noreply.github.com>
2021-05-12 09:56:14 +0530

    Add files via upload

---
+++ diary.py
@@ -0,0 +1,8 @@
+print("Enter the Unique Code (case sensitive):")
+c = input("")
+if c == "26MA":
+    print("https://mitadmissions.org/blogs/entry/a-special-announcement-about-sat-subject-tests/")
+elif c == "11TM":
+    print("http://catalog.mit.edu/mit/undergraduate-education/general-institute-requirements/")
+elif c == "27tA":
+    print("https://github.com/AkashShanmugaraj/Projects/tree/master/Register%20Screen")
